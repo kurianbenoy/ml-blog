0e77a3c53a781bdf5b6260dbf522965a5475004c
Sourcery AI <>
2022-02-21 15:45:24 +0000

    'Refactored by Sourcery'

--- _action_files/fast_template.py
+++ _action_files/fast_template.py
@@ -14,16 +14,14 @@ def rename_for_jekyll(nb_path: Path, warnings: Set[Tuple[str, str]]=None) -> str
     """
     assert nb_path.exists(), f'{nb_path} could not be found.'
 
-    # Checks if filename is compliant with Jekyll blog posts
-    if _re_blog_date.match(nb_path.name): return nb_path.with_suffix('.md').name.replace(' ', '-')
-    
-    else:
-        clean_name = _re_numdash.sub('', nb_path.with_suffix('.md').name).replace(' ', '-')
+    if _re_blog_date.match(nb_path.name):
+        if _re_blog_date.match(nb_path.name): return nb_path.with_suffix('.md').name.replace(' ', '-')
+    clean_name = _re_numdash.sub('', nb_path.with_suffix('.md').name).replace(' ', '-')
 
-        # Gets the file's last modified time and and append YYYY-MM-DD- to the beginning of the filename
-        mdate = os.path.getmtime(nb_path) - 86400 # subtract one day b/c dates in the future break Jekyll
-        dtnm = datetime.fromtimestamp(mdate).strftime("%Y-%m-%d-") + clean_name
-        assert _re_blog_date.match(dtnm), f'{dtnm} is not a valid name, filename must be pre-pended with YYYY-MM-DD-'
-        # push this into a set b/c _nb2htmlfname gets called multiple times per conversion
-        if warnings: warnings.add((nb_path, dtnm))
-        return dtnm
+    # Gets the file's last modified time and and append YYYY-MM-DD- to the beginning of the filename
+    mdate = os.path.getmtime(nb_path) - 86400 # subtract one day b/c dates in the future break Jekyll
+    dtnm = datetime.fromtimestamp(mdate).strftime("%Y-%m-%d-") + clean_name
+    assert _re_blog_date.match(dtnm), f'{dtnm} is not a valid name, filename must be pre-pended with YYYY-MM-DD-'
+    # push this into a set b/c _nb2htmlfname gets called multiple times per conversion
+    if warnings: warnings.add((nb_path, dtnm))
+    return dtnm
